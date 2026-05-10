0946b7d60a6fd8fe34d24880232fbe12dac63c48
Shota Matsuda <shota@takram.com>
2026-05-11 08:46:46 +0900

    Use water area in cityscape story

--- storybook-webgpu/src/atmosphere/Cityscape.stories.tsx
+++ storybook-webgpu/src/atmosphere/Cityscape.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta } from '@storybook/react-vite'
+import { TilesPlugin } from '3d-tiles-renderer/r3f'
 
 import { createStory } from '../components/createStory'
+import { WaterAreaTilesOverlay } from '../plugins/waterArea/WaterAreaImageOverlay'
+import { WaterAreaNodeMaterial } from '../plugins/waterArea/WaterAreaNodeMaterial'
+import { WaterAreaOverlayPlugin } from '../plugins/waterArea/WaterAreaOverlayPlugin'
 import { Story } from './3DTilesRenderer-Shadows'
 
 import StoryCode from './3DTilesRenderer-Shadows?raw'
@@ -20,15 +24,26 @@ export default {
 
 export const Cityscape = createStory(Story, {
   props: {
-    longitude: -73.9709,
-    latitude: 40.7589,
-    heading: -155,
-    pitch: -35,
-    distance: 3000,
-    csmFar: 1e4
+    longitude: -73.9883,
+    latitude: 40.7657,
+    heading: -42,
+    pitch: -25,
+    distance: 2667,
+    fov: 50,
+    csmFar: 1e4,
+    materialHandler: () => new WaterAreaNodeMaterial(),
+    globeChildren: (
+      <TilesPlugin
+        plugin={WaterAreaOverlayPlugin}
+        args={{
+          overlays: [new WaterAreaTilesOverlay()],
+          enableTileSplitting: false
+        }}
+      />
+    )
   },
   args: {
-    toneMappingExposure: 60,
+    toneMappingExposure: 40,
     dayOfYear: 1,
     timeOfDay: 7.6
   },
